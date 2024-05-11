795ae110cae2d6abcba199357faa21fb29b8152b
Tiko7454 <tiko7454av@gmail.com>
2024-05-11 15:08:51 +0400

    complated the project structure

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from mic import record, output_filename
 from recognize import recognize
+from command import transform_output, Command, read_number
 
 
 while True:
     record()
     output = recognize(output_filename)
+    print(output)
+    if not Command.head_command:
+        command = transform_output(output)
+        Command.add_command(command)
+    else:
+        number = read_number(output)
+        Command.add_argument(number)
+    if Command.is_complete():
+        assert Command.head_command
+        Command.head_command.execute()
+        Command.clean()
