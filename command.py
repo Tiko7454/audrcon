fc9044a967cc4b20a027656c3692a99bf62a17ec
Izabella <atajanyanizabella@gmail.com>
2024-05-13 14:07:14 +0400

    refactored

--- command.py
+++ command.py
@@ -239,7 +239,7 @@ def read_number(word: str) -> int:
         "hundred": 100,
         "thousand": 1000,
     }
-    words = word.lower().split()
+    words = word.lower().replace("-", " ").split()
     number = 0
     temp_number = 0
     for word in words:
@@ -248,10 +248,72 @@ def read_number(word: str) -> int:
         if word in numbers:
             temp_number += numbers[word]
         elif word == "hundred":
-            temp_number *= 100
-        elif word == "thousand":
+            temp_number += 100
             number += temp_number * 1000
             temp_number = 0
+        elif word == "thousand":
+            temp_number *= 1000
+            number += temp_number * 1000
+            temp_number = 0
+        else:
+            raise ValueError("Invalid number")
+    number += temp_number
+    return number
+
+
+def read_number(word: str) -> int:
+    numbers = {
+        "zero": 0,
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9,
+        "ten": 10,
+        "eleven": 11,
+        "twelve": 12,
+        "thirteen": 13,
+        "fourteen": 14,
+        "fifteen": 15,
+        "sixteen": 16,
+        "seventeen": 17,
+        "eighteen": 18,
+        "nineteen": 19,
+        "twenty": 20,
+        "thirty": 30,
+        "forty": 40,
+        "fifty": 50,
+        "sixty": 60,
+        "seventy": 70,
+        "eighty": 80,
+        "ninety": 90,
+        "hundred": 100,
+        "thousand": 1000,
+    }
+    words = word.lower().replace("-", " ").split()
+    number = 0
+    temp_number = 0
+    for word in words:
+        if word == "and":
+            continue
+        if word == "hundred":
+            if number == 0:
+                number = 100
+            else:
+                number += temp_number * 100
+                temp_number = 0
+        elif word == "thousand":
+            if number == 0:
+                number = 1000
+            else:
+                number += temp_number * 1000
+                temp_number = 0
+        elif word in numbers:
+            temp_number += numbers[word]
         else:
             raise ValueError("Invalid number")
     number += temp_number
